69e7cd7d49dde1bd54bc1b48a4d73b250233af0c
Philipp Kessling <p.kessling@leibniz-hbi.de>
2024-12-05 10:27:31 +0100

    UTC-aware reset intervals

--- spiderexpress_tiktok/spiderexpress_tiktok.py
+++ spiderexpress_tiktok/spiderexpress_tiktok.py
@@ -1,12 +1,19 @@
-import functools
-from typing import Dict, List, Tuple
+"""This module contains the SpiderExpress plugins for fetching TikTok data.
+
+Constants:
+    call_limits (Dict): The call limits for the TikTok API.
+    ACCESS_TOKEN (AccessToken): The access token for the TikTok API.
+"""
+
 import datetime
+import functools
+import time
+from typing import Dict, List, Tuple
 
 import pandas as pd
-import time
+from loguru import logger
 from researchtikpy import AccessToken, get_followers, get_following, get_users_info
 from spiderexpress import PlugIn
-from loguru import logger
 
 call_limits = {
     "followers": 20000,
@@ -20,29 +27,29 @@ _call_counter_ = {
     "users_info": 0,
 }
 
-_access_token_ = None
+ACCESS_TOKEN = None
 
 
 def _get_access_token_(key: str, secret: str) -> AccessToken:
     """This function returns the access token for the TikTok API.
-    
+
     Returns:
         AccessToken: The access token for the TikTok API.
     """
-    global _access_token_
-    if _access_token_ is None:
+    global ACCESS_TOKEN  # pylint: disable=W0603
+    if ACCESS_TOKEN is None:
         logger.info("Requesting new access token.")
-        _access_token_ = AccessToken(
+        ACCESS_TOKEN = AccessToken(
             client_key=key,
             client_secret=secret,
         )
-    logger.info("Using access token: {_access_token_}.")
-    return _access_token_
+    logger.info("Using access token: {ACCESS_TOKEN}.")
+    return ACCESS_TOKEN
 
 
 def _reset_date_() -> datetime.datetime:
     """This function returns the next reset time for the TikTok API.
-    
+
     Returns:
         datetime.datetime: The next reset time for the TikTok API.
     """
@@ -52,20 +59,20 @@ def _reset_date_() -> datetime.datetime:
     if now >= noon:  # If it is past 12am, then the next reset time is tomorrow
         return noon + datetime.timedelta(days=1)
     return noon
-    
+
 
 def _get_reset_seconds_(reset_date: datetime.datetime) -> int:
     """This function returns the number of seconds until the next reset time for the TikTok API.
-    
+
     Returns:
         int: The number of seconds until the next reset time for the TikTok API.
     """
-    return (reset_date - datetime.datetime.now()).total_seconds()
+    return (reset_date - datetime.datetime.now(datetime.UTC)).total_seconds()
 
 
 def guard_end_point(endpoint: str):
     """This function is a decorator that guards the TikTok API endpoints.
-    
+
     Parameters:
         endpoint (str): The TikTok API endpoint to guard.
 
@@ -79,23 +86,27 @@ def guard_end_point(endpoint: str):
         raise ValueError(f"Invalid endpoint: {endpoint}")
 
     def decorator(func):
-        
+
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             if _call_counter_[endpoint] >= call_limits[endpoint]:
                 reset_date = _reset_date_()
                 seconds_until_reset = _get_reset_seconds_(reset_date)
 
-                logger.info(f"Waiting for {seconds_until_reset} seconds until reset at {reset_date.isoformat()}")
-                
+                logger.info(
+                    f"Waiting for {seconds_until_reset} seconds until"
+                    "reset at {reset_date.isoformat()}"
+                )
+
                 time.sleep(seconds_until_reset)
-                
+
                 _call_counter_[endpoint] = 0
             else:
                 _call_counter_[endpoint] += 1
             return func(*args, **kwargs)
+
         return wrapper
-    
+
     return decorator
 
 
@@ -107,8 +118,11 @@ def _users_info_(handle: str, token: AccessToken) -> pd.DataFrame:
     data["name"] = handle
     return data
 
+
 @guard_end_point("followers")
-def _followers_(handles: List[str], configuration: Dict) -> Tuple[pd.DataFrame, pd.DataFrame]:
+def _followers_(
+    handles: List[str], configuration: Dict
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """This is a SpiderExpress plugin entrypoint for fetching followers"""
     token = _get_access_token_(
         configuration["client_key"],
@@ -119,19 +133,24 @@ def _followers_(handles: List[str], configuration: Dict) -> Tuple[pd.DataFrame, 
         handles, token, max_count=100, total_count=total_count
     )
     all_nodes = (
-        pd.concat([all_followers.username, all_followers.target_account])
-        .unique()
-        .tolist()
-    ) if configuration.get("fetch_all", False) else handles
-    user_info = pd.concat([
-        _users_info_(handle=handle, token=token)
-        for handle in all_nodes
-    ])
+        (
+            pd.concat([all_followers.username, all_followers.target_account])
+            .unique()
+            .tolist()
+        )
+        if configuration.get("fetch_all", False)
+        else handles
+    )
+    user_info = pd.concat(
+        [_users_info_(handle=handle, token=token) for handle in all_nodes]
+    )
     return all_followers, user_info
 
 
 @guard_end_point("followings")
-def _followings_(handles: List[str], configuration: Dict) -> Tuple[pd.DataFrame, pd.DataFrame]:
+def _followings_(
+    handles: List[str], configuration: Dict
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """This is a SpiderExpress plugin entrypoint for fetching followings"""
     token = _get_access_token_(
         configuration["client_key"],
@@ -139,17 +158,22 @@ def _followings_(handles: List[str], configuration: Dict) -> Tuple[pd.DataFrame,
     )
     # total_count = config.get("total_count", 1500)
     all_followings = get_following(
-        handles, token, max_count=100,  # total_count=total_count
+        handles,
+        token,
+        max_count=100,  # total_count=total_count
     )
     all_nodes = (
-        pd.concat([all_followings.username, all_followings.target_account])
-        .unique()
-        .tolist()
-    ) if configuration.get("fetch_all", False) is True else handles
-    user_info = pd.concat([
-        _users_info_(handle=handle, token=token)
-        for handle in all_nodes
-    ])
+        (
+            pd.concat([all_followings.username, all_followings.target_account])
+            .unique()
+            .tolist()
+        )
+        if configuration.get("fetch_all", False) is True
+        else handles
+    )
+    user_info = pd.concat(
+        [_users_info_(handle=handle, token=token) for handle in all_nodes]
+    )
     return all_followings, user_info
 
 
